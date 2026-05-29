3d39ede38232a97aac4091b36c9e3d3dbdf99e3e
Everton Pessoa <evertonpesssoa@gmail.com>
2026-05-29 17:32:27 -0300

    Atualizar o script.js

--- mini-mundos/global/sistema_solar/script.js
+++ mini-mundos/global/sistema_solar/script.js
@@ -1,14 +1,27 @@
 function createStars() {
   const container = document.querySelector("body");
   for (let i = 0; i < 1000; i++) {
-    // Increase the number of stars to 1000
     const star = document.createElement("div");
     star.className = "star";
-    star.style.width = ".1px";
-    star.style.height = ".1px";
+    
+    // Tamanho um pouco maior e variado (1px a 3px)
+    const size = Math.random() * 2 + 1;
+    star.style.width = size + "px";
+    star.style.height = size + "px";
+    
+    // Posição aleatória
     star.style.top = Math.random() * 100 + "%";
     star.style.left = Math.random() * 100 + "%";
+    
+    // Opacidade variada (algumas mais brilhantes, outras mais fracas)
+    star.style.opacity = Math.random() * 0.7 + 0.3;
+    
+    // Animação com delay aleatório
+    star.style.animationDelay = Math.random() * 5 + "s";
+    
     container.appendChild(star);
   }
 }
-createStars();
+
+// Aguarda o CSS carregar completamente
+window.addEventListener("DOMContentLoaded", createStars);
