a7d4a65707fca36e6b8608b58900933cca000dae
Everton Pessoa <evertonpesssoa@gmail.com>
2026-08-12 23:08:58 -0300

    Atualizar o main.js

--- mini-mundos/global/3D-art-gallery-threejs/main.js
+++ mini-mundos/global/3D-art-gallery-threejs/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 import { scene, setupScene } from "./modules/scene.js";
 import { createPaintings } from "./modules/paintings.js";
 import { createWalls } from "./modules/walls.js";
